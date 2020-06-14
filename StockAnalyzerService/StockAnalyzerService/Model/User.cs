747b10cd81994962b73ba56c9a4ac7432689bd6b
Harsh Jain <jainh9999@gmail.com>
2020-06-13 22:35:37 -0400

    added models for use by services

---
+++ StockAnalyzerService/StockAnalyzerService/Model/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StockAnalyzerService.Model {
+    public class User {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
