9bf1e5dbf2db88e79ebcd573d842093429816d48
Harsh Jain <jainh9999@gmail.com>
2020-06-13 22:36:00 -0400

    Custom http get call that returns an object

---
+++ StockAnalyzerService/StockAnalyzerService/Service/HttpCalls.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace StockAnalyzerService.Service {
+    public class HttpCalls: IHttpCalls {
+        public HttpCalls() { }
+
+        public async Task<T> Get<T>(HttpClient httpClient, string urlParameters) {
+            try {
+                string url = httpClient.BaseAddress + urlParameters;
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode) {
+                    return await response.Content.ReadAsAsync<T>();
+                }
+                return default;
+            } catch {
+                return default;
+            }
+
+        }
+    }
+}
---
+++ StockAnalyzerService/StockAnalyzerService/Service/IHttpCalls.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAnalyzerService.Service {
+    public interface IHttpCalls {
+        Task<T> Get<T>(HttpClient httpClient, string urlParameters);
+    }
+}
