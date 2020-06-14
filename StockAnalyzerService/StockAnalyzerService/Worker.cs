1930d854cba363831373c4789c87a7e8e3076044
Harsh Jain <jainh9999@gmail.com>
2020-06-13 22:35:03 -0400

    trying to get logging to log to a file

--- StockAnalyzerService/StockAnalyzerService/Worker.cs
+++ StockAnalyzerService/StockAnalyzerService/Worker.cs
@@ -4,24 +4,25 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace StockAnalyzerService
 {
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
+        // private readonly ILogger _logger;
 
-        public Worker(ILogger<Worker> logger)
-        {
-            _logger = logger;
-        }
+        // public Worker(ILogger<Worker> logger)
+        // {
+        //     _logger = logger;
+        // }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                
+                Log.Information("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
         }
