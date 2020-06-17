c7bd3cfa0465eb4fd7394d3f546b06b5ae0ef16a
Harsh Jain <jainh9999@gmail.com>
2020-06-17 18:42:27 -0400

    split up configuration into multiple methods

--- StockAnalyzerService/StockAnalyzerService/Program.cs
+++ StockAnalyzerService/StockAnalyzerService/Program.cs
@@ -18,27 +18,35 @@ namespace StockAnalyzerService {
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(loggingBuilder => {
-                    var configuration = new ConfigurationBuilder()
-                        .SetBasePath(Directory.GetCurrentDirectory())
-                        .AddJsonFile("appsettings.json")
-                        .Build();
-                    var logger = new LoggerConfiguration()
-                        .ReadFrom.Configuration(configuration)
-                        .CreateLogger();
-                    // loggingBuilder.AddSerilog(logger, dispose: true);
-                })
-                .ConfigureServices((hostContext, services) => {
-                    services.AddHostedService<Worker>();
-                    services.AddLogging(l => l.AddSerilog());
-                    services.AddHttpClient("stockAnalyzer", c => {
-                        c.BaseAddress = new Uri("http://localhost:3000/api/");
-                        c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    });
-                    services.AddSingleton<IStockAnalyzer, StockAnalyzer>();
-                    services.AddSingleton<IHttpCalls, HttpCalls>();
-                }).UseWindowsService();
+            var host = Host.CreateDefaultBuilder(args);
+            host = ConfigureLogging(host);
+            host = ConfigureServices(host);
+            return host.UseWindowsService();
+        }
+
+        public static IHostBuilder ConfigureLogging(IHostBuilder host) {
+            return host.ConfigureLogging(configureLogging => {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+            });
+        }
+
+        public static IHostBuilder ConfigureServices(IHostBuilder host) {
+            return host.ConfigureServices((hostContext, services) => {
+                services.AddHostedService<Worker>();
+                services.AddLogging(l => l.AddSerilog());
+                services.AddHttpClient("stockAnalyzer", c => {
+                    c.BaseAddress = new Uri("http://localhost:3000/api/");
+                    c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                });
+                services.AddSingleton<IStockAnalyzer, StockAnalyzer>();
+                services.AddSingleton<IHttpCalls, HttpCalls>();
+            });
         }
     }
 }
