5aa9ebb4b9a493ac327bba456e596464a813708b
Nishant Jain <jainn9999@gmail.com>
2020-06-26 23:33:48 -0400

    removing test data to make sure flow to the database is created

--- StockAnalyzerService/Worker.cs
+++ StockAnalyzerService/Worker.cs
@@ -13,35 +13,16 @@ namespace StockAnalyzerService
     public class Worker : BackgroundService
     {
         private readonly ILogger _logger;
-        private readonly IStockAnalyzer _analyzer;
 
-        public Worker(ILogger<Worker> logger, IStockAnalyzer analyzer)
+        public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
-            _analyzer = analyzer;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
-            {
-                StockCandlestickViewModel data = new StockCandlestickViewModel()
-                {
-                    Ticker = "MSFT",
-                    Candlesticks = new List<Candlestick>()
-                    {
-                        new Candlestick()
-                        {
-                            HighPrice = 10,
-                            LowPrice = 5,
-                            ClosePrice = 8,
-                            OpenPrice = 3
-                        }
-                    }
-                };
-
-                _analyzer.PostCandlestickData(data);
-                
+            {   
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
