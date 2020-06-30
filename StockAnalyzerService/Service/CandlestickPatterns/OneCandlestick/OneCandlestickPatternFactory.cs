a56258cc7d9b36e7d7b61086048eb973bc03ec17
Harsh Jain <jainh9999@gmail.com>
2020-06-30 16:40:42 -0400

    put the dictionary init in the constructor

--- StockAnalyzerService/Service/CandlestickPatterns/OneCandlestick/OneCandlestickPatternFactory.cs
+++ StockAnalyzerService/Service/CandlestickPatterns/OneCandlestick/OneCandlestickPatternFactory.cs
@@ -3,18 +3,14 @@ using StockAnalyzerService.Model;
 
 namespace StockAnalyzerService.Service {
     public class OneCandlestickPatternFactory {
-        private Dictionary<IOneCandlestickPattern, StockAction> _PatternDict;
+        public Dictionary<IOneCandlestickPattern, StockAction> PatternDict { get; set; }
 
         public OneCandlestickPatternFactory() {
-            _PatternDict = new Dictionary<IOneCandlestickPattern, StockAction>();
-        }
-        public Dictionary<IOneCandlestickPattern, StockAction> CreatePatterns() {
-            _PatternDict.Add(new ShootingStar(), StockAction.Buy);
-            _PatternDict.Add(new InvertedHammer(), StockAction.Buy);
-            _PatternDict.Add(new Hammer(), StockAction.Sell);
-            _PatternDict.Add(new HangingMan(), StockAction.Sell);
-
-            return _PatternDict;
+            PatternDict = new Dictionary<IOneCandlestickPattern, StockAction>();
+            PatternDict.Add(new ShootingStar(), StockAction.Buy);
+            PatternDict.Add(new InvertedHammer(), StockAction.Buy);
+            PatternDict.Add(new Hammer(), StockAction.Sell);
+            PatternDict.Add(new HangingMan(), StockAction.Sell);
         }
     }
 }
