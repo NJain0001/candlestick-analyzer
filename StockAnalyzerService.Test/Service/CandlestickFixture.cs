f05b32fbeb9a6b03d24c4060220bd2785a8ade5c
Harsh Jain <jainh9999@gmail.com>
2020-08-09 14:20:41 -0400

    fixture for shared data now has actual data

--- StockAnalyzerService.Test/Service/CandlestickFixture.cs
+++ StockAnalyzerService.Test/Service/CandlestickFixture.cs
@@ -11,16 +11,91 @@ namespace StockAnalyzerService.Test.Service {
         public List<double> downwardTrendLine;
         public List<double> flatTrendLine;
         public CandlestickFixture() {
-            upTrendCandlesticks = new List<Candlestick>();
-            downTrendCandlesticks = new List<Candlestick>();
-            flatTrendCandlesticks = new List<Candlestick>();
-            upwardTrendLine = new List<double>();
-            downwardTrendLine = new List<double>();
-            flatTrendLine = new List<double>();
+            upTrendCandlesticks = populateUpTrendCandlesticks();
+            downTrendCandlesticks = populateDownTrendCandlesticks();
+            flatTrendCandlesticks = populateFlatTrendCandlesticks();
+            upwardTrendLine = populateUpwardTrendLine();
+            downwardTrendLine = populateDownwardTrendLine();
+            flatTrendLine = populateFlatwardTrendLine();
         }
 
         public void Dispose() {
 
+        }
+
+        public List<Candlestick> populateUpTrendCandlesticks() {
+            List<Candlestick> returnList = new List<Candlestick>();
+            for (int i = 0; i < 400; i++) {
+                Candlestick candlestick = new Candlestick() {
+                    OpenPrice = i / 2.0,
+                    ClosePrice = i / 2.0,
+                    HighPrice = i / 2.0,
+                    LowPrice = i / 2.0
+                };
+
+                returnList.Add(candlestick);
+            }
+
+            return returnList;
+        }
+
+        public List<Candlestick> populateDownTrendCandlesticks() {
+            List<Candlestick> returnList = new List<Candlestick>();
+            for (int i = 400; i > 0; i--) {
+                Candlestick candlestick = new Candlestick() {
+                    OpenPrice = i / 2.0,
+                    ClosePrice = i / 2.0,
+                    HighPrice = i / 2.0,
+                    LowPrice = i / 2.0
+                };
+
+                returnList.Add(candlestick);
+            }
+
+            return returnList;
+        }
+
+        public List<Candlestick> populateFlatTrendCandlesticks() {
+            List<Candlestick> returnList = new List<Candlestick>();
+            for (int i = 0; i < 400; i++) {
+                Candlestick candlestick = new Candlestick() {
+                    OpenPrice = i / 3.0,
+                    ClosePrice = i / 3.0,
+                    HighPrice = i / 3.0,
+                    LowPrice = i / 3.0
+                };
+
+                returnList.Add(candlestick);
+            }
+
+            return returnList;
+        }
+
+        public List<double> populateUpwardTrendLine() {
+            List<double> returnList = new List<double>();
+            for (double i = 0.0; i <= 100.0; i+=.5) {
+                returnList.Add(i+49.75);
+            }
+
+            return returnList;
+        }
+
+        public List<double> populateDownwardTrendLine() {
+            List<double> returnList = new List<double>();
+            for (double i = 100.0; i >= 0.0; i-=.5) {
+                returnList.Add(i+49.75);
+            }
+
+            return returnList;
+        }
+
+        public List<double> populateFlatwardTrendLine() {
+            List<double> returnList = new List<double>();
+            for (double i = 0.0; i <= 100.0; i+=.5) {
+                returnList.Add(45.00);
+            }
+
+            return returnList;
         }
     }
 }
